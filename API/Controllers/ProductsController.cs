2fe0b3825cddbe0bfb6a8bfb6e31851c3308a6ae
stefan.sadovek <stefan.sadovek@fincore.com>
2020-07-08 21:54:14 +0200

    added comment

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,6 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    /*
+        ApiController attribute:
+            - has a lot of logic under the hood for us, and one is PARAMETER VALIDATION
+            - [HttpGet("{id}")] <--- if we pass string as a parameter here, it will throw
+              a BadRequest error
+    */
     [ApiController]
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
